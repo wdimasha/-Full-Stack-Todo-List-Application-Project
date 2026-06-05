24b1108068324e676b38a1792870a2fd97fc1b88
Amalki Imasha <a.imashafernando99@gmail.com>
2026-06-05 21:35:57 +0530

    added toggle and delte functions,also styled all with css

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -4,10 +4,7 @@ import com.app.todoapp.models.Task;
 import com.app.todoapp.service.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +25,16 @@ public class TaskController {
     @PostMapping
     public String createTask(@RequestParam String title){
         taskService.createTask(title);
+        return "redirect:/";
+    }
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id) {
+        taskService.toggleTask(id);
         return "redirect:/";
     }
 }
--- src/main/java/com/app/todoapp/service/TaskService.java
+++ src/main/java/com/app/todoapp/service/TaskService.java
@@ -22,6 +22,18 @@ public class TaskService {
     public void createTask(String title) {
         Task task = new Task();
         task.setTitle(title);
+        task.setCompleted(false);
+        taskRepository.save(task);
+    }
 
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public void toggleTask(Long id) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("Invalid task id"));
+        task.setCompleted(!task.isCompleted());
+        taskRepository.save(task);
     }
 }
